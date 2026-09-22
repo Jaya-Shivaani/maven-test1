3c8d24545598f4b7a6040b03477fb67e10f86623
Jaya Shivaani <jayashivaani2005@gmail.com>
2026-09-22 16:41:32 +0530

    SHIVAANIS0001 - Created HelloWorld Program

--- src/main/java/com/example/javamavenjunithelloworld/Shivaani.java
+++ src/main/java/com/example/javamavenjunithelloworld/Shivaani.java
@@ -1,8 +1,8 @@
 package com.example.javamavenjunithelloworld;
 
-public class Steve {
+public class Shivaani{
 
     public static void main(String[] args) {
-        System.out.println("I am Steve");
+        System.out.println("I am Shivaani");
     }
 }
